2d4eab8861e91cd952af537dd4131f0f481a1fa2
Nhlakanipho Masilela <nhlakanipho.masilela@boxfusion.io>
2026-03-17 15:56:20 +0200

    Refactor code structure for improved readability and maintainability

---
+++ aspnet-core/src/ABPGroup.Application/Persons/PersonAppService.cs
@@ -0,0 +1,38 @@
+using Abp.Application.Services;
+using Abp.Authorization;
+using Abp.Domain.Repositories;
+using Abp.Linq.Extensions;
+using ABPGroup.Authorization;
+using ABPGroup.Authorization.Users;
+using ABPGroup.Persons.Dto;
+using System.Linq;
+
+namespace ABPGroup.Persons
+{
+    [AbpAuthorize(PermissionNames.Pages_Persons)]
+    public class PersonAppService
+        : AsyncCrudAppService<User, PersonDto, long, PagedPersonResultRequestDto, CreateUpdatePersonDto, CreateUpdatePersonDto>,
+          IPersonAppService
+    {
+        public PersonAppService(IRepository<User, long> repository) : base(repository)
+        {
+            CreatePermissionName = PermissionNames.Pages_Persons_Create;
+            UpdatePermissionName = PermissionNames.Pages_Persons_Edit;
+            DeletePermissionName = PermissionNames.Pages_Persons_Delete;
+        }
+
+        protected override IQueryable<User> CreateFilteredQuery(PagedPersonResultRequestDto input)
+        {
+            return Repository.GetAll()
+                .WhereIf(!string.IsNullOrEmpty(input.Keyword),
+                    x => x.UserName.Contains(input.Keyword) ||
+                         x.DisplayName.Contains(input.Keyword) ||
+                         x.GitHubUsername.Contains(input.Keyword));
+        }
+
+        protected override IQueryable<User> ApplySorting(IQueryable<User> query, PagedPersonResultRequestDto input)
+        {
+            return query.OrderByDescending(x => x.CreationTime);
+        }
+    }
+}
